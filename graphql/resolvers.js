d82198ce12bbfe7d3caf5511c3937998f53e71d1
Bahaa Abou Yassine <bahaa.abouyassine2@gmail.com>
2023-10-06 10:58:38 +0300

    ✅ Input validation!

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,32 @@
 const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
+const validator = require("validator").default;
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
     const email = userInput.email;
     const name = userInput.name;
     const password = userInput.password;
+    const errors = [];
+    if (!validator.isEmail(email)) {
+      errors.push({ message: "Email is invalid" });
+    }
+    if (
+      validator.isEmpty(password) ||
+      validator.isLength(password, { min: 6 })
+    ) {
+      errors.push({ message: "Password is too short" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
 
     if (await User.findOne({ email: email })) {
-      const error = new Error("User exists already!");
+      const error = new Error("User already exists!");
       throw error;
     }
 
