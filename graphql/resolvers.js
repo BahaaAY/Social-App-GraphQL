afeb876e8027547016619f633b55166cfd8bf2fb
Bahaa Abou Yassine <bahaa.abouyassine2@gmail.com>
2023-10-06 10:29:11 +0300

    ➕ Creating user with GraphQL Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,27 @@
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+
 module.exports = {
-  hello() {
-    return {
-      text: "Hello World!",
-      views: 1245,
-    };
+  createUser: async function ({ userInput }, req) {
+    const email = userInput.email;
+    const name = userInput.name;
+    const password = userInput.password;
+
+    if (await User.findOne({ email: email })) {
+      const error = new Error("User exists already!");
+      throw error;
+    }
+
+    const hashedPw = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      name: name,
+      password: hashedPw,
+    });
+
+    const createdUser = await user.save();
+
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -2,16 +2,41 @@ const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(
   `
-    type TestData {
-        text: String!
-        views: Int!
+  #graphql
+   
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
+}
+    type User {
+        _id: ID!
+        email: String!
+        name: String!
+        password: String
+        posts: [Post!]!
     }
-    type RootQuery {
-        hello : TestData!
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
     }
 
+    type RootQuery {
+        hello: String
+    }
     schema {
+        
+    
         query: RootQuery
+       mutation: RootMutation
     }
 `
 );
